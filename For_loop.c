eccc878c4b8a69069c168c6d71fac7c885a98a97
SATYA PRANEETH KOUSHIK SRIPADA <praneethk9912@gmail.com>
2025-11-11 08:56:02 +0530

    Create For_loop.c

---
+++ For_loop.c
@@ -0,0 +1,16 @@
+//concept of the for loop
+
+#include<stdio.h>
+int main ()
+{
+    int n =100;
+    int i;
+    
+    for(i=0;i<=n;i++)
+    {
+        printf(" the value of i is %d \n", i);
+    }
+
+return 0;
+}
+
